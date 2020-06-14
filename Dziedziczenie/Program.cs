7a49ef6217fa5bfd64bd4b2382682c12296ce657
sebnorth <sebnorth@gmail.com>
2020-06-14 08:46:35 +0200

    Klasa Osoba plus testy

--- Dziedziczenie/Program.cs
+++ Dziedziczenie/Program.cs
@@ -10,7 +10,37 @@ namespace Dziedziczenie
     {
         static void Main(string[] args)
         {
+            #region testy cz.1
 
+            Osoba o = new Osoba("Adam", "Miś", "20.03.1980");
+            Osoba o2 = new Student("Michał", "Kot", "13.04.1990", 2, 1, 12345);
+            Osoba o3 = new Pilkarz("Mateusz", "Żbik", "10.08.1986", "obrońca", "FC Częstochowa");
+
+            o.WypiszInfo();
+            o2.WypiszInfo();
+            o3.WypiszInfo();
+
+            Student s = new Student("Krzysztof", "Jeż", "22.12.1990", 2, 5, 54321);
+            Pilkarz p = new Pilkarz("Piotr", "Kos", "14.09.1984", "napastnik", "FC Politechnika");
+
+            s.WypiszInfo();
+            p.WypiszInfo();
+
+            ((Pilkarz)o3).StrzelGola();
+
+            p.StrzelGola();
+            p.StrzelGola();
+
+            o3.WypiszInfo();
+            p.WypiszInfo();
+            Console.ReadKey();
+
+
+            #endregion
+
+            #region testy cz.2
+
+            #endregion
         }
     }
 }
