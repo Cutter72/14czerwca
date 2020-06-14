8911db19e2faf285f172cde3a5d9f99039a725eb
sebnorth <sebnorth@gmail.com>
2020-06-14 09:59:02 +0200

    Kod startowy do zadania 14 czerwca, ININ2_B3.2

--- Dziedziczenie/Student.cs
+++ Dziedziczenie/Student.cs
@@ -11,6 +11,7 @@ namespace Dziedziczenie
         private int rok;
         private int grupa;
         private int nrIndeksu;
+        List<Ocena> oceny = new List<Ocena>();
 
         public Student(string imie_, string nazwisko_, string dataUrodzenia_, int rok_, int grupa_, int nrIndeksu_) : base(imie_, nazwisko_, dataUrodzenia_) {
             this.rok = rok_;
@@ -22,6 +23,55 @@ namespace Dziedziczenie
         {
             base.WypiszInfo();
             Console.WriteLine($"Dodatkowe informacje: rok {this.rok}, grupa {this.grupa}, nr indeksu {this.nrIndeksu}");
+
+            Console.WriteLine($"Oceny studenta: ");
+            foreach (Ocena ocena in oceny)
+            {
+                ocena.WypiszInfo();
+            }
         }
+
+        #region metody obsługujące oceny
+
+        public void DodajOcene(string nazwaPrzedmiotu, string data, double wartosc)
+        {
+            Ocena ocena = new Ocena(nazwaPrzedmiotu, data, wartosc);
+            oceny.Add(ocena);
+        }
+
+        public void UsunOcene(string nazwaPrzedmiotu, string data, double wartosc) {
+
+            for (int i = 0; i < oceny.Count;)
+            {
+                Ocena o = oceny[i];
+                if (o.NazwaPrzedmiotu == nazwaPrzedmiotu && o.Data == data && o.Wartosc == wartosc)
+                {
+                    oceny.RemoveAt(i);
+                }
+
+                else
+                {
+
+                    ++i;
+                } 
+            }
+        }
+
+        public void WypiszOceny() {
+            // TODO 28 czerwca
+        }
+
+        public void WypiszOceny(string nazwaPrzedmiotu) {
+            // TODO 28 czerwca
+        }
+
+        public void UsunOceny() {
+            // TODO 28 czerwca
+        }
+
+        public void UsunOceny(string nazwaPrzedmiotu) {
+            // TODO 28 czerwca
+        }
+        #endregion
     }
 }
